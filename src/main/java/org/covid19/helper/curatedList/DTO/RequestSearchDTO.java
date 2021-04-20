6c6d3d5d069acfd44e31aa21adbbb70e3e70a124
Rachit Puri <rachitpuri@Rachits-MacBook-Air.local>
2021-04-20 18:53:07 +0530

    Search Fix

--- src/main/java/org/covid19/helper/curatedList/DTO/RequestSearchDTO.java
+++ src/main/java/org/covid19/helper/curatedList/DTO/RequestSearchDTO.java
@@ -39,16 +39,16 @@ public class RequestSearchDTO {
         if (this.isSeeker != null) {
                 q = q+  " is_giver = " + !this.isSeeker + " and";
         }
-        if(this.city != null && !"".equals(this.city)){
+        if(this.city != null && !"null".equals(this.city)){
             q = q+" city = '" + this.city + "' and";
         }
 //        if(this.state != null ){
 //            q = q+ " state = '" + this.state + "' and";
 //        }
-        if(this.requestType != null && !"".equals(this.requestType)){
+        if(this.requestType != null && !"null".equals(this.requestType)){
             q = q+ " request_type = '" + this.requestType + "' and";
         }
-        if(this.q != null && !"".equals(this.q)){
+        if(this.q != null && !"null".equals(this.q)){
             q = q+ "( name like '%" +this.q +"%' or phone like '%" + this.q +
                     "%' or address like '%" + this.q + "%' ) and";
         }
